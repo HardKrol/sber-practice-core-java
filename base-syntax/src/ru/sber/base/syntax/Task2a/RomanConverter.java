07f49b7d7f8d39ffd5847035318bd584caad1793
HardKrol <Toha.zelenin00@gmail.com>
2025-03-11 08:54:56 +0300

    Задание 2а только числа от 1 до 100

--- base-syntax/src/ru/sber/base/syntax/Task2a/RomanConverter.java
+++ base-syntax/src/ru/sber/base/syntax/Task2a/RomanConverter.java
@@ -8,14 +8,16 @@ public class RomanConverter {
             throw new IllegalArgumentException("Введите число от 1 до 100");
         }
 
-        String[] thousands = { "", "M", "MM", "MMM" };
-        String[] hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+//        String[] thousands = { "", "M", "MM", "MMM" };
+//        String[] hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+        String[] hundreds = { "", "C" };
         String[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
         String[] units = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
         StringBuilder roman = new StringBuilder();
-        roman.append(thousands[number / 1000]);
-        roman.append(hundreds[(number % 1000) / 100]);
+//        roman.append(thousands[number / 1000]);
+//        roman.append(hundreds[(number % 1000) / 100]);
+        roman.append(hundreds[number / 100]);
         roman.append(tens[(number % 100) / 10]);
         roman.append(units[number % 10]);
 
